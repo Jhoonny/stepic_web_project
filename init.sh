66dc89dbfa21e24112f505000783088a598f6267
Johnny <i.lupasco@gmail.com>
2016-02-28 22:05:24 +0200

    ini for firs test

--- init.sh
+++ init.sh
@@ -2,10 +2,15 @@
 
 # /home/box/web
 
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+# При входе в терминал надо запустить Ngnix и проверить:
+# sudo /etc/init.d/nginx start
+# ps -o pid,euser,egroup,comm,args -C nginx
+
+#sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
 
 #git clone https://github.com/Jhoonny/stepic_web_project.git /home/box/web
 #bash /home/box/web/init.sh

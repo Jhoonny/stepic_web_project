348ea27bef44c85acc6a78690c44688b825036f1
Johnny <i.lupasco@gmail.com>
2016-02-28 22:33:15 +0200

    new config server

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@
 # sudo /etc/init.d/nginx start
 # ps -o pid,euser,egroup,comm,args -C nginx
 
-#sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ﻿ln -sf /home/box/web/etc/nginx2.conf  /etc/nginx/sites-enabled/test.conf
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test

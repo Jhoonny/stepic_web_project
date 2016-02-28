5618ba7b5854a3c4d9f555cc41e43d4234f89372
Johnny <i.lupasco@gmail.com>
2016-02-28 21:30:02 +0200

    File ini work

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# /home/box/web
+
+#sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+
+#git clone https://github.com/your_account/stepic_web_project.git /home/box/web
+#bash /home/box/web/init.sh

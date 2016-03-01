6b3dba82a99d6abdbbd306d70116d86fd200ca94
Johnny <i.lupasco@gmail.com>
2016-03-01 15:39:34 +0200

    restructure code

--- init.sh
+++ init.sh
@@ -5,10 +5,8 @@
 sudo /etc/init.d/nginx start
 # ps -o pid,euser,egroup,comm,args -C nginx
 
-#sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo ln -sf /home/box/web/etc/nginx3.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/gunicorn.conf
 sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart

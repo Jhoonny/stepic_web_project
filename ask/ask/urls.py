74913a456f8deb4d54010f08db8a70f677283513
Johnny <i.lupasco@gmail.com>
2016-03-04 12:26:19 +0200

    edit config for urls

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+"""
+old config urls
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -20,9 +22,22 @@ urlpatterns = [
     url(r'', sites.urls),
     url(r'^login/', login.site.urls),
     url(r'^signup/', signup.site.urls),
-    url(r'^question/{}/', question.site.urls),
+    url(r'^question/(\d+)/', question.site.urls),
     url(r'^ask/', ask.site.urls),
     url(r'^popular/', popular.site.urls),
     url(r'^new/', new.site.urls),
-
 ]
+"""
+from django.conf.urls import url, patterns
+
+ulrpatterns = patterns("qa.views",
+                       url("^$", "test", name="first_page"),
+                       url("^login/$", "test", name="login"),
+                       url("^signup/$", "test", name="signup"),
+                       url("^question/(?P<pk>\d+)/$", "test", name="question"),
+                       url("^ask/", "test", name="ask"),
+                       url("^popular/$", "test", name="popular"),
+                       url("^new/$", "test", name="new")
+                       )
+
+)

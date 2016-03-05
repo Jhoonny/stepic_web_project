3cbd9d30bcf7be77b4df13b63379ebd85470ced8
Johnny <i.lupasco@gmail.com>
2016-03-05 13:54:18 +0200

    edit views

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, Http404, HttpRequest
 
 def test(request, *args, **kwargs):
   return HttpResponse('OK')
   # return render(request, "base.html")
+
+def post_text(request):
+  try:
+    id = request.GET.get('id')
+    obj = Post.objects.get(pk=id)
+  except Post.DoesNotExist:
+    raise Http404
+  return HttpResponse(obj.text, content_type='text/plain')
+

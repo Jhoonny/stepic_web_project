4874d1aa1a0fde26cc38af97c22921f48e1ab45f
Johnny <i.lupasco@gmail.com>
2016-03-06 17:08:53 +0200

    edit model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,23 +4,27 @@ from django.contrib.auth.models import User
 class Question(models.Model):
   title = models.CharField(max_length=255)
   text = models.TextField()
-  added_at = models.DateTimeField(blank=True)
-  rating = models.IntegerField()
-  author = models.CharField(max_length=50)
-  likes = models.ManyToManyField(User)
+  added_at = models.DateTimeField(null=False, auto_now_add=True)
+  rating = models.IntegerField(null=False, default=0)
+  author = models.ForeignKey(User, related_name='user_author')
+  likes = models.ManyToManyField(User, related_name='user_likes')
 
   def __unicode__(self):
     return self.title
+
   def get_absolute_url(self):
     return '/question/%d/' % self.pk
 
   class Meta:
-    db_table = 'qa_question'
+    db_table = 'question'
     ordering = ['-creation_date']
 
 
 class Answer(models.Model):
   text = models.TextField()
-  added_at = models.DateTimeField(blank=True)
-  author = models.CharField(max_length=50)
+  added_at = models.DateTimeField(null=False, auto_now_add=True)
   question = models.ForeignKey(Question)
+  author = models.ForeignKey(User)
+
+  def __unicode__(self):
+    return self.title

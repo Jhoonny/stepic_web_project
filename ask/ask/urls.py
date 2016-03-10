87b208d43c97e3bd1848ebd132d98a93bb03015e
Johnny <i.lupasco@gmail.com>
2016-03-10 20:59:24 +0200

    test for lesson 2.6

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,35 +1,9 @@
 # ask URL Configuration
-"""
-from django.conf.urls import url
-from ask.qa import views
-
-urlpatterns = (\
-   url(r'^$', views.test, name='test'),
-   url(r'^login/', views.test, name='login'),
-   url(r'^signup/', views.test, name='signup'),
-   url(r'^question/\d+/', views.test, name='question'),
-   url(r'^ask/', views.test, name='aks'),
-   url(r'^popular/', views.test, name='popular'),
-   url(r'^new/', views.test, name='new'),
-
-   url(r'^(?P<slug>\w+)/$',views.question_list , name='question_list'),
-   url(r'^popular/', , name=''),
-   url(r'^question/(?P<id>\d+)/$', views., name=''),
-)
-"""
-from django.conf.urls import url
-try:
-    from stepic_web.ask.qa import views
-except ImportError:
-    import sys
-    sys.path.append("/home/box")
-    from web.ask.qa import views
+from django.conf.urls import url, include
+from django.contrib import admin
 
 
-urlpatterns = (url("^$", views.get_questions, name="first_page", kwargs={"question_type": "new"}),
-               url("^login/$",  views.test, name="login"),
-               url("^signup/$",  views.test, name="signup"),
-               url("^question/(?P<id>\d+)/$",  views.get_current_question, name="question"),
-               url("^ask/",  views.ask_question, name="ask"),
-               url("^popular/$",  views.get_questions, name="popular", kwargs={"question_type": "popular"}),
-               url("^new/$",  views.get_questions, name="new", kwargs={"question_type": "new"}))
+urlpatterns = [
+    url(r'^', include('qa.urls')),
+    url(r'^admin/', admin.site.urls),
+]

ff5cbbcafcaac5462e4f3016c4393e694ef21a18
Johnny <i.lupasco@gmail.com>
2016-03-10 13:04:49 +0200

    edit bd update

--- bd.sh
+++ bd.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 sudo /etc/init.d/mysql start
-mysql -uroot -e "CREATE DATABASE ask_db"
-mysql -uroot -e "CREATE USER 'ask'@'localhost' IDENTIFIED BY PASSWORD 'YES'"
-mysql -uroot -e "GRANT ALL ask_db.* TO 'ask'@'localhost' IDENTIFIED BY PASSWORD 'YES'"
-sudo /etc/init.d/mysql restart
+sudo mysql -uroot -e "CREATE DATABASE ask_db"
+sudo mysql -uroot -e "CREATE USER 'ask'@'localhost' IDENTIFIED BY PASSWORD 'YES'"
+sudo mysql -uroot -e "GRANT ALL ask_db.* TO 'ask'@'localhost' IDENTIFIED BY PASSWORD 'YES'"
+#sudo /etc/init.d/mysql restart
 python /home/box/web/ask/manage.py syncdb
 python /home/box/web/ask/manage.py validate

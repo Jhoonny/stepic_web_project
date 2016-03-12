15e03d45fcb766d866a82498eb259ef649f55404
Johnny <i.lupasco@gmail.com>
2016-03-12 22:53:11 +0200

    to run python3

--- aux_soft.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get update
-#sudo apt-get remove python-django
-sudo pip3 install django
-sudo pip3 install gunicorn
-sudo apt-get install python3-dev libmysqlclient-dev
-sudo pip3 install mysqlclient
-

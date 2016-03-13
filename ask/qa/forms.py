fdea664fc60c2320186f12c9f01226ecf637a609
Johnny <i.lupasco@gmail.com>
2016-03-13 17:57:42 +0200

    update form adn models

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -6,16 +6,18 @@ class AskForm(forms.Form):
   title = forms.CharField(max_length=255)
   text = forms.CharField(widget=forms.Textarea)
 
-  def __init__(self, user=None, **kwargs):
-    self.user = user
+  # def __init__(self, user=None, **kwargs):
+  #   self._user = user
+  #   super(AskForm, self).__init__(**kwargs)
+  def __init__(self, *args, **kwargs):
     super(AskForm, self).__init__(*args, **kwargs)
 
   # def clean(self):
   #   cleaned_data = super(AskForm, self).clean()
 
-    # def save(self):
-    #   self.cleaned_data['author'] = self.user
-    #   return Question.objects.create(**self.cleaned_data)
+  # def save(self):
+  #   self.cleaned_data['author'] = self.user
+  #   return Question.objects.create(**self.cleaned_data)
 
   def save(self):
     self.cleaned_data["author"] = self.user
@@ -28,8 +30,10 @@ class AnswerForm(forms.Form):
   text = forms.CharField(widget=forms.Textarea)
   question = forms.ModelChoiceField(queryset=Question.objects.all())
 
-  def __init__(self, user=None, **kwargs):
-    self.user = user
+  # def __init__(self, user=None, **kwargs):
+  #   self._user = user
+  #   super(AnswerForm, self).__init__(**kwargs)
+  def __init__(self, *args, **kwargs):
     super(AnswerForm, self).__init__(*args, **kwargs)
 
   # def clean(self):
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
   title = models.CharField(max_length=255)
   text = models.TextField()
   added_at = models.DateField(null=True, auto_now_add=True)
-  rating = models.IntegerField(null=Truee, default=0)
+  rating = models.IntegerField(null=True, default=0)
   author = models.ForeignKey(User, related_name='user_author', null=True, default=1)
   likes = models.ManyToManyField(User, related_name='user_likes', null=True)
 
@@ -16,9 +16,9 @@ class Question(models.Model):
 
 class Answer(models.Model):
   text = models.TextField()
-  added_at = models.DateField(null=Truee, auto_now_add=True)
-  question = models.ForeignKey(Question, null=True1)
-  author = models.ForeignKey(User, related_name='user_author', null=True, default=1)
+  added_at = models.DateField(null=True, auto_now_add=True)
+  question = models.ForeignKey(Question, null=True)
+  author = models.ForeignKey(User, null=True, default=1)
 
   def __unicode__(self):
     return self.text

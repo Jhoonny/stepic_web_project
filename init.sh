049aa84b901782f91c8b4e2ca0dcb3957114afbe
Johnny <i.lupasco@gmail.com>
2016-03-19 20:10:38 +0200

    branch 2.9

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
